afc29087ab833dbb06e905f29d684d59e756a614
fsoc <8392143a@gmail.com>
2017-12-24 12:23:07 +0100

    Change static serving.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const MONGODB_STRING = process.env.NODE_ENV === 'dev'
   ? 'mongodb://root:admin@ds161306.mlab.com:61306/memoriae'
   : 'mongodb://root:' + process.env.PASS + '@ds161346.mlab.com:61346/memoriae_prod'
 
+const path = require('path')
+
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -22,7 +24,8 @@ const db = mongoose.connection
 db.on('error', console.error)
 db.once('open', () => console.log('connected to db!'))
 
-process.env.NODE_ENV !== 'dev' && app.use(express.static('app/build'))
+if (process.env.NODE_ENV !== 'dev')
+  app.use(express.static(path.join(__dirname, 'app/build/')))
 
 app.use('/api', require('./routes/api'))
 

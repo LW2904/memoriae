ef34c824bbff0383070e831bd8d879bc3f7d8b80
fsoc <8392143a@gmail.com>
2018-02-17 13:50:56 +0100

    Implement auth and error handling.

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,13 @@ import * as bodyParser from 'koa-bodyparser';
 app.use(bodyParser());
 app.use(session({}, app));
 
+import flash from './middleware/flash';
+app.use(flash());
+
+import passport from './auth';
+app.use(passport.initialize());
+app.use(passport.session());
+
 import { join } from 'path';
 import * as serve from 'koa-static';
 app.use(serve(join(__dirname, '/public')));
@@ -36,8 +43,34 @@ app.use(async (ctx, next) => {
   )(ctx, next);
 });
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    app.emit('error', err); // Preserve default behavior.
+
+    ctx.status = ctx.status || 500;
+    await ctx.render('status', {
+      message: err.message,
+    });
+  }
+});
+
 import router from './router';
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const server = app.listen(8080);
+const port = process.env.PORT || '8008';
+const server = app.listen(port);
+debug('server listening on %o', port);
+
+const { DB_URL, DB_PASS, DB_USER } = process.env;
+if (!(DB_URL && DB_PASS && DB_USER)) {
+  throw new Error('Missing database connection parameters.');
+}
+
+// Connect to database.
+import * as mongoose from 'mongoose';
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_URL}`)
+  .then(() => debug('connected to database'))
+  .catch((err) => debug('database connection error: %O', err.message));
